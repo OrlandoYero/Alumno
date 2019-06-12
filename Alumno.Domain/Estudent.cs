184ed62fe9e7668afbae694838db3cb265177e92
Orlando Yero Cabrales <orlando9104@gmail.com>
2019-06-12 18:36:04 -0400

    Se agrega el modelo para los estudiantes y se ingresa lógica inicial para la lectura de xml

---
+++ Alumno.Domain/Estudent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alumno.Domain
+{
+    public class Estudent
+    {
+        public string Name { get; set; }
+        public string MotherLastName { get; set; }
+        public string FatherLastName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public int Grade { get; set; }
+        public string Group { get; set; }
+        public float Calification { get; set; }
+    }
+}
