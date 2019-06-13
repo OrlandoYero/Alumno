df7868cc07803a28fa531f57da51890f2790642d
Orlando Yero Cabrales <orlando9104@gmail.com>
2019-06-12 22:19:33 -0400

    Se implementa lógica para la optención de todas las hojas de la página excel

---
+++ Alumno.BusinessLogic/Implementation/ReaderXls.cs
@@ -0,0 +1,50 @@
+﻿using Alumno.Domain;
+using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alumno.BusinessLogic.Implementation
+{
+    public class ReaderXls
+    {
+        public static List<Estudent> ReadFile(string fileName, string page, string size)
+        {
+            using (var stream = File.Open(fileName, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream)) {
+                    do
+                    {
+                        // get header
+                        var header = reader.Read();
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader.GetString(0) + " " + reader.GetString(1) + " " + reader.GetString(2) + " " + Convert.ToDateTime(reader[3].ToString()));
+                        }
+                    } while (reader.NextResult());
+                }
+            }
+            return null;
+        }
+
+        public static List<string> GetSheetNames(string fileName) {
+            List<string> SheetNames = new List<string>();
+            using (var stream = File.Open(fileName, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    do
+                    {
+                        SheetNames.Add(reader.Name);
+                        
+                    } while (reader.NextResult());
+                }
+            }
+            return SheetNames;
+        }
+    }
+}
---
+++ Alumno.Test/BusinessLogicTest/ReaderXlsTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Alumno.BusinessLogic.Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Alumno.Test.BusinessLogicTest
+{
+    [TestClass]
+    public class ReaderXlsTest
+    {
+        [TestMethod]
+        public void ExisteFileCalif()
+        {
+            Assert.IsTrue(File.Exists(@"Data\Calificaciones.xlsx"));
+        }
+
+        [TestMethod]
+        public void LoadDataXls() {
+            ReaderXls.ReadFile(@"Data\Calificaciones.xlsx", null, null);
+        }
+
+        [TestMethod]
+        public void GetHeadersXls()
+        {
+            ReaderXls.GetHeaders(@"Data\Calificaciones.xlsx");
+        }
+    }
+}
