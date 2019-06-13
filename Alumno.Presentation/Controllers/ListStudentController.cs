d22f91460ca0011e3e306e2756bb80bf1d23510a
Orlando Yero Cabrales <orlando9104@gmail.com>
2019-06-13 18:36:55 -0400

    Se adiciono controller, tabla y acción rotar

---
+++ Alumno.Presentation/Controllers/ListStudentController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alumno.Presentation.Controllers
+{
+    public class ListStudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
