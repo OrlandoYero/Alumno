f6a1911832ccaf3209aa99cb25bc413fb27daa62
Orlando Yero Cabrales <orlando9104@gmail.com>
2019-06-16 15:20:55 -0400

    Corrigiendo test key generate

--- Alumno.Test/BusinessLogicTest/KeyGenerateTest.cs
+++ Alumno.Test/BusinessLogicTest/KeyGenerateTest.cs
@@ -11,10 +11,10 @@ namespace Alumno.Test.BusinessLogicTest
         [TestMethod]
         public void GetPositionInAlphabetTest()
         {
-            Console.WriteLine(KeyGenerate.GetCharacterInAlpha('a')); //X
-            Console.WriteLine(KeyGenerate.GetCharacterInAlpha('n')); // K
-            Console.WriteLine(KeyGenerate.GetCharacterInAlpha('e')); // B
-            Console.WriteLine(KeyGenerate.GetCharacterInAlpha('s')); // P
+            Assert.AreEqual("X",KeyGenerate.GetCharacterInAlpha('a'));
+            Assert.AreEqual("K",KeyGenerate.GetCharacterInAlpha('n'));
+            Assert.AreEqual("B",KeyGenerate.GetCharacterInAlpha('e'));
+            Assert.AreEqual("P", KeyGenerate.GetCharacterInAlpha('s'));
         }
 
         [TestMethod]
@@ -23,16 +23,17 @@ namespace Alumno.Test.BusinessLogicTest
             {
                 Birthdate = DateTime.Parse("30/04/1991")
             });
-            Console.WriteLine(age);
+            var testCreatedDelay = (new DateTime(1, 1, 1) + (DateTime.Now - Convert.ToDateTime("30/04/2019"))).Year - 1;
+            Assert.AreEqual(28 + testCreatedDelay, age);
         }
 
         [TestMethod]
         public void GetTwoLettersTest() {
-            Console.WriteLine(KeyGenerate.GetTwoLetters(""));
-            Console.WriteLine(KeyGenerate.GetTwoLetters("a"));
-            Console.WriteLine(KeyGenerate.GetTwoLetters("abc"));
-            Console.WriteLine(KeyGenerate.GetTwoLetters("Angel", true));
-            Console.WriteLine(KeyGenerate.GetTwoLetters("Robles"));
+            Assert.AreEqual("--", KeyGenerate.GetTwoLetters(""));
+            Assert.AreEqual("a-", KeyGenerate.GetTwoLetters("a"));
+            Assert.AreEqual("bc", KeyGenerate.GetTwoLetters("abc"));
+            Assert.AreEqual("An", KeyGenerate.GetTwoLetters("Angel", true));
+            Assert.AreEqual("es", KeyGenerate.GetTwoLetters("Robles"));
         }
 
         [TestMethod]
@@ -40,10 +41,11 @@ namespace Alumno.Test.BusinessLogicTest
             var student = new StudentData()
             {
                 Name = "Angel",
+                FatherLastName = "Moreno",
                 MotherLastName = "Robles",
                 Birthdate = DateTime.Parse("20/05/1997")
             };
-            Console.WriteLine(KeyGenerate.GenerateStudentKey(student));
+            Assert.AreEqual("XKBP22", KeyGenerate.GenerateStudentKey(student));
         }
     }
 }
